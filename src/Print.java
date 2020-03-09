55d72f479d1877758bbbd1c5e2f8e475497bae61
Minh Cao Duy <s3818272@maclabsst-base.rmit.edu.vn>
2020-03-09 16:10:59 +0700

    Initial commit

---
+++ src/Print.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+
+
+
+public class Print {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+        char ch = 'a';
+        int in = ch;
+        System.out.println(in);
+
+
+    }
+}
